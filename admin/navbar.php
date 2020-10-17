04652a46b3f7dbb08a81fd862a379f0bcbc0f5c3
Destiny Brotobor <destinybravos@gmail.com>
2020-10-17 13:11:22 +0100

    Created Admin Part for the e-market

---
+++ admin/navbar.php
@@ -0,0 +1,31 @@
+<div class="container-fluid bg-white shadow-sm"  id="top_menu">
+    <nav class="navbar navbar-expand navbar-light">
+        <ul class="nav navbar-nav">
+            <li class="nav-item">
+                <a href="#" class="nav-link text-dark px-3">
+                    <i class="fa fa-user-check"></i>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link text-dark px-3">
+                    <i class="fa fa-file-alt"></i>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link text-dark px-3">
+                    <i class="fa fa-pen"></i>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link text-dark px-3">
+                    <i class="fa fa-edit"></i>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link text-dark px-3">
+                    <i class="fa fa-layer-group"></i>
+                </a>
+            </li>
+        </ul>
+    </nav>
+</div>
